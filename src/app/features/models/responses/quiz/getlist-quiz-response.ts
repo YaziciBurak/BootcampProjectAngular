4211f1ac033d8b28c93a2e3a7c76d7b9e91f6d4a
Burak Yazıcı <133709862+YaziciBurak@users.noreply.github.com>
2024-05-02 23:11:37 +0300

    admin dashboard crud operations

--- src/app/features/models/responses/quiz/getlist-quiz-response.ts
+++ src/app/features/models/responses/quiz/getlist-quiz-response.ts
@@ -1,7 +1,10 @@
 export interface GetlistQuizResponse {
     id:number;
     applicantId:string;
+    applicantFirstName:string;
+    applicantLastName:string;
     bootcampId:number;
+    bootcampName:string;
     startTime:Date;
     endTime:Date;
 }
