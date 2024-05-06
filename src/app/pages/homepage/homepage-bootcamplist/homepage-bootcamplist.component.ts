4b78df7797482ddbdcf4663d5947cd8ade03aeb7
GÜLSÜM ORAN GÜNEŞ <avgulsumoran@gmail.com>
2024-05-06 11:15:31 +0300

    responsive fixes

--- src/app/pages/homepage/homepage-bootcamplist/homepage-bootcamplist.component.ts
+++ src/app/pages/homepage/homepage-bootcamplist/homepage-bootcamplist.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { BootcampCardComponent } from '../../../shared/components/bootcamp-card/bootcamp-card.component';
 
 @Component({
   selector: 'app-homepage-bootcamplist',
   standalone: true,
-  imports: [BootcampCardComponent],
+  imports: [RouterModule, CommonModule, BootcampCardComponent],
   templateUrl: './homepage-bootcamplist.component.html',
-  styleUrl: './homepage-bootcamplist.component.css'
+  styleUrl: './homepage-bootcamplist.component.css',
 })
-export class HomepageBootcamplistComponent {
-
-}
+export class HomepageBootcamplistComponent {}
--- src/app/pages/homepage/homepage-second/homepage-second.component.ts
+++ src/app/pages/homepage/homepage-second/homepage-second.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+type FeaturedCard = {
+  title: string;
+  description: string;
+  imagePath: string;
+};
 
 @Component({
   selector: 'app-homepage-second',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './homepage-second.component.html',
-  styleUrl: './homepage-second.component.css'
+  styleUrl: './homepage-second.component.css',
 })
 export class HomepageSecondComponent {
-
+  featuredCards: FeaturedCard[] = [
+    {
+      title: 'Uzman Eğitmenler',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting',
+      imagePath: '/assets/img/homepage-feature-card-1.svg',
+    },
+    {
+      title: 'Öğretici İçerikler',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting',
+      imagePath: '/assets/img/homepage-feature-card-2.svg',
+    },
+    {
+      title: 'Sertifika Fırsatı',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting',
+      imagePath: '/assets/img/homepage-feature-card-3.svg',
+    },
+  ];
 }
