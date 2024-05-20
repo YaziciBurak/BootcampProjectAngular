fa0cfb9aad21b5c1ae8a9358b826cf4920a4977b
GÜLSÜM ORAN GÜNEŞ <avgulsumoran@gmail.com>
2024-05-21 00:01:48 +0300

    vercel build scripti eklendi

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+rm environments/environment.development.ts
+
+cat <<EOF > environments/environments.ts
+export const environment = {API_URL:'$API_URL'}
+EOF
+
+npm run build
