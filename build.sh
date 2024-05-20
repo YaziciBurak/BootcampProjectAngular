2ab960d52128e584acd5df0b8bdde7ba5059724a
GÜLSÜM ORAN GÜNEŞ <avgulsumoran@gmail.com>
2024-05-21 02:03:55 +0300

    login 405 hatası

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+cat <<EOF > src/environments/environment.ts
+export const environment = {API_URL:'$API_URL'}
+EOF
+
 cat <<EOF > src/environments/environment.development.ts
 export const environment = {API_URL:'$API_URL'}
 EOF
