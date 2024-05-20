ff04d1ca5956ad1c80bfa1563eb80bdd38372852
GÜLSÜM ORAN GÜNEŞ <avgulsumoran@gmail.com>
2024-05-20 15:11:57 +0300

    tüm assest ler eklendi - icon,image vs artık hiçbirini dışarıdan çekmiyoruz

--- src/app/pages/homepage/homepage-ticker/homepage-ticker.component.ts
+++ src/app/pages/homepage/homepage-ticker/homepage-ticker.component.ts
@@ -20,51 +20,52 @@ export class HomepageTickerComponent {
     {
       name: 'Elastic',
       imageUrl:
-        'https://cdn.builder.io/api/v1/image/assets/TEMP/13149432d5758d20387fd0e10649aacd4dccc4a62aa581b2144b92d6845c2880?apiKey=d64f73725dfb4ccb8c714d58677398b5&',
-      extraClasses: 'w-60 max-w-full aspect-[3.03]',
+        '/assets/img/elastic-logo.svg',
+      extraClasses: 'w-144 h-47',
     },
     {
       name: 'Hubspot',
       imageUrl:
-        'https://cdn.builder.io/api/v1/image/assets/TEMP/08a5cdcdd7b112cca4dcc4e376ac9cf23a91f04ef05a787a7d901281f6c930d8?apiKey=d64f73725dfb4ccb8c714d58677398b5&',
-      extraClasses: 'aspect-[2.86]',
+        '/assets/img/hubspot-logo.png',
+      extraClasses: 'w-144 h-47',
     },
     {
       name: 'Click Up',
       imageUrl:
-        'https://cdn.builder.io/api/v1/image/assets/TEMP/d941b4836d8a85314b4e09a9b6ab1368b158e23cdd4581fda8e3168b1c5bdae6?apiKey=d64f73725dfb4ccb8c714d58677398b5&',
-      extraClasses: 'aspect-[2]',
+        '/assets/img/clickup-logo.png',
+      extraClasses: 'w-144 h-50',
+    },
+    {
+      name: 'Github',
+      imageUrl:
+        '/assets/img/github-logo.png',
+      extraClasses: 'aspect-[2.44]',
     },
     {
       name: 'Dropbox',
       imageUrl:
-        'https://cdn.builder.io/api/v1/image/assets/TEMP/93dcc2fc9751bf8d1d2dc33133c16b5eab06e163ed6ab6f2772906d9d7d55c6a?apiKey=d64f73725dfb4ccb8c714d58677398b5&',
-      extraClasses: 'aspect-[2]',
+        '/assets/img/dropbox-logo.png',
+      extraClasses: 'w-144 h-47',
     },
-    
-    {
-      name: 'Github',
-      imageUrl:
-        'https://cdn.builder.io/api/v1/image/assets/TEMP/16bfc557e96702087bfe9e6dea6eb1629e348256f9ab9245d440e47446f39e7d?apiKey=d64f73725dfb4ccb8c714d58677398b5&',
-      extraClasses: 'aspect-[2.44]',
-    },
+
     {
       name: 'FreshBooks',
       imageUrl:
-        'https://cdn.builder.io/api/v1/image/assets/TEMP/c34ef1b30ce64c703b3c4a9a84a17b84523fdd5a7c235da600c235d4637a5478?apiKey=d64f73725dfb4ccb8c714d58677398b5&',
+        '/assets/img/freshbooks-logo.png',
       extraClasses: 'w-60 aspect-[3.45]',
-    },
-    {
-      name: 'Google',
-      imageUrl:
-        'https://cdn.builder.io/api/v1/image/assets/TEMP/fbc79f49a95aa09b5d1e0e9fb23cb067874c7de14ae7da2e08ce06ebcbe5ab38?apiKey=d64f73725dfb4ccb8c714d58677398b5&',
-      extraClasses: 'aspect-[2.33] w-[185px]',
     },
     {
       name: 'Elastic',
       imageUrl:
-        'https://cdn.builder.io/api/v1/image/assets/TEMP/13149432d5758d20387fd0e10649aacd4dccc4a62aa581b2144b92d6845c2880?apiKey=d64f73725dfb4ccb8c714d58677398b5&',
-      extraClasses: 'w-60 max-w-full aspect-[3.03]',
+        '/assets/img/elastic-logo.svg',
+      extraClasses: 'w-144 h-47',
     },
+    {
+      name: 'Google',
+      imageUrl:
+        '/assets/img/google-logo.svg',
+      extraClasses: 'w-144 h-47',
+    },
+
   ];
 }
