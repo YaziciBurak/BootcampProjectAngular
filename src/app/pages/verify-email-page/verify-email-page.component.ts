bdaaa07f4cae30fb8072b98bc5ea4e7efe6feed6
GÜLSÜM ORAN GÜNEŞ <avgulsumoran@gmail.com>
2024-05-26 14:30:45 +0300

    email verify eklendi

---
+++ src/app/pages/verify-email-page/verify-email-page.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../features/services/concretes/auth.service';
+
+@Component({
+  selector: 'app-verify-email-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './verify-email-page.component.html',
+  styleUrl: './verify-email-page.component.css'
+})
+export class VerifyEmailPageComponent implements OnInit {
+  activationKey: string;
+  success: boolean | null = null;
+
+  constructor(private route: ActivatedRoute, private authService: AuthService) {
+    this.route.queryParams.subscribe((params) => {
+      this.activationKey = params['ActivationKey'];
+    });
+  }
+
+  ngOnInit(): void {
+    console.log('VerifyEmailComponent', this.activationKey);
+    this.authService
+      .verifyEmail({ authenticatorCode: this.activationKey })
+      .subscribe(
+        (response) => {
+          console.log('VerifyEmailComponent', response);
+          this.success = true;
+        },
+        (error) => {
+          console.log('VerifyEmailComponent', error);
+          this.success = false;
+        }
+      );
+  }
+
+}
